74b649425a715b83f031a4938caf96c8d83a7548
Chreld <christian.eld@tuta.io>
2025-08-18 16:57:52 +0200

    Updated player movements to include WASD

--- player.gd
+++ player.gd
@@ -8,17 +8,15 @@ extends CharacterBody2D
 func _physics_process(delta):
 	var input_dir = Vector2.ZERO
 
-	# Horizontal input
-	if Input.is_action_pressed("ui_right"):
-		input_dir.x += 1
-	if Input.is_action_pressed("ui_left"):
-		input_dir.x -= 1
-	
-	# Vertical control
+	# User input
+	if Input.is_action_pressed("ui_up"):
+		input_dir.y -= 1
 	if Input.is_action_pressed("ui_down"):
 		input_dir.y += 1
-	elif Input.is_action_pressed("ui_up"):
-		input_dir.y -= 1
+	if Input.is_action_pressed("ui_left"):
+		input_dir.x -= 1
+	if Input.is_action_pressed("ui_right"):
+		input_dir.x += 1
 
 	# Apply buoyancy (natural upward drift) when NOT pressing down
 	if not Input.is_action_pressed("ui_down"):
